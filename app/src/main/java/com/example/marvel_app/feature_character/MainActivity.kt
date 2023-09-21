39b9ad1a92b3496092e7223f8c6e5692fb1c0b57
Francisco Lisboa <francisco.lisboa@poatek.com>
2023-09-21 16:29:03 -0300

    import in main activity

--- app/src/main/java/com/example/marvel_app/feature_character/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app/feature_character/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.marvel_app.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
